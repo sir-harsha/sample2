585a461d694b1645e499051b7e80c7229958e8f9
sri-harsha <sriharsha.b@datagrokr.com>
2024-09-03 13:40:47 +0530

    Create testing.py

---
+++ testing.py
@@ -0,0 +1,12 @@
+import unittest
+from addi import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+    
+    def setUp(self):
+        self.calc = SimpleCalculator()
+
+    def test_add(self):
+        self.assertEqual(self.calc.add(7, 8), 15)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(0, 0), 0)
