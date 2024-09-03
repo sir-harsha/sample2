6708647b89f3e06527df59ecf9310f5d4531277d
sir-harsha <sriharsha.b@datagrokr.com>
2024-09-03 22:32:32 +0530

    pre

--- tests/testingaddi.py
+++ tests/testingaddi.py
@@ -9,4 +9,7 @@ class TestSimpleCalculator(unittest.TestCase):
     def test_add(self):
         self.assertEqual(self.calc.add(7, 8), 15)
         self.assertEqual(self.calc.add(-1, 1), 0)
-        self.assertEqual(self.calc.add(0, 0), 0)
+        self.assertEqual(self.calc.add(0, 0), 0)\
+            
+if __name__ == '__main__':
+    unittest.main()
